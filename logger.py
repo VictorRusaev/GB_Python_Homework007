7140ccc7eef71a70f1a6e9a2395ed9ab56fddc71
Victor Rusaev <rusaev74@mail.ru>
2022-12-30 06:40:55 +0300

    Added encoding

--- logger.py
+++ logger.py
@@ -4,6 +4,6 @@ def add_contact():
     name = input('Введите имя: ')
     phone = input('Введите номер телефона в формате +70000000000: ')
     time = dt.now().strftime('%D %H:%M')
-    with open('Homeworks\Homework007\phonebook.txt', 'a') as file:
+    with open('Homeworks\Homework007_1\phonebook.txt', 'a', encoding='utf-8') as file:
         file.write('Date created: {};\nName: {};\nPhone: {};\n  \n'
                     .format(time, name, phone))
